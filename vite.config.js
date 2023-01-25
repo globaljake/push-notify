74c9acd500c3fed8a9c4a3e3b24a71ec1cc1a57d
Jake Quattrocchi <4119460+globaljake@users.noreply.github.com>
2023-01-24 21:24:14 -0500

    feat: set up build and deploy

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,19 @@ import elmPlugin from "vite-plugin-elm";
 export default defineConfig({
   plugins: [elmPlugin({})],
   publicDir: "public",
-  // build: {
-  //   emptyOutDir: false,
-  //   outDir: "../priv/static",
-  //   manifest: false,
-  //   brotliSize: true,
-  //   rollupOptions: {
-  //     input: {
-  //       app: "src/index.js",
-  //     },
-  //     output: {
-  //       assetFileNames: "assets/[name].[ext]",
-  //       entryFileNames: "assets/[name].js",
-  //     },
-  //   },
-  // },
+  build: {
+    emptyOutDir: false,
+    outDir: "dist",
+    manifest: false,
+    brotliSize: true,
+    rollupOptions: {
+      input: {
+        app: "src/index.js",
+      },
+      output: {
+        assetFileNames: "assets/[name].[ext]",
+        entryFileNames: "assets/[name].js",
+      },
+    },
+  },
 });
